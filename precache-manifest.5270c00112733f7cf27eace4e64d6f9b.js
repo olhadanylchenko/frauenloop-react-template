08f5f6c4a5ed0f5a7de77fd5d85c20e21a13c03e
Olha Danylchenko <olhadanylchenko@MacBook-Air-Olha.local>
2020-02-03 20:09:56 +0100

    Updates

---
+++ precache-manifest.5270c00112733f7cf27eace4e64d6f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "746d78ec99b7c60d050ad39b1265f291",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c225a1940f2d9ffdb7c",
+    "url": "./static/css/main.310393ae.chunk.css"
+  },
+  {
+    "revision": "a5cbd1ded5ca90dcff3f",
+    "url": "./static/js/2.bd09e5e6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.bd09e5e6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5c225a1940f2d9ffdb7c",
+    "url": "./static/js/main.2694db34.chunk.js"
+  },
+  {
+    "revision": "6f1cb0f1d4d9c8208063",
+    "url": "./static/js/runtime-main.d6441537.js"
+  }
+]);
